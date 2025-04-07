569207a0b762b14accfb90735bc76393131b2459
a940731 <a940731@DESKTOP-KT3CS8K>
2025-04-07 15:29:20 +0200

    Commit inicial

---
+++ ClubNautico/src/test/java/com/practicas/springjpa/ClubNauticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicas.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubNauticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
